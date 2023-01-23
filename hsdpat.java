2c81220f0749bc29e86f23eb9ba18918abbcac23
sqigglyhema <113290948+sqigglyhema@users.noreply.github.com>
2023-01-23 14:06:54 +0530

    Create hsdpat.java

---
+++ hsdpat.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class hsdpat{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n,i,j;
+System.out.print("Enter size of square:");
+n=sc.nextInt();
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+if((i==0)||(j==0)||(i==(n-1))||(j==(n-1)))
+System.out.print("$ ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}
+}
+}
+Footer
