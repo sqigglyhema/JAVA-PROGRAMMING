a22b5725dd6a9821add25a794a993702ceec771b
sqigglyhema <113290948+sqigglyhema@users.noreply.github.com>
2023-01-23 14:37:24 +0530

    Create sqcn.java

---
+++ sqcn.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class sqcun{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double n;
+System.out.print("Enter number:");
+n=sc.nextDouble();
+System.out.println("Square:"+(n*n));
+System.out.println("Cube:"+(n*n*n));
+}
+}
