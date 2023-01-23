ffb5c9bcbcf935e1761e471038f856ea1df524fe
sqigglyhema <113290948+sqigglyhema@users.noreply.github.com>
2023-01-23 13:35:40 +0530

    Create ODD.java

---
+++ ODD.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class odd{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n,v;
+System.out.print("Enter n value:");
+n=sc.nextInt();
+v=(n*4)-1;
+System.out.println(n+" odd number after "+n+" odd numbers is:"+v);
+}}
